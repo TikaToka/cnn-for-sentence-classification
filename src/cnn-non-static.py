93bed4047d95f934b9c889563728a22fbeae25a4
TikaToka <ford0103@naver.com>
2021-07-30 14:19:04 +0900

    optimized mem

--- src/cnn-non-static.py
+++ src/cnn-non-static.py
@@ -1,3 +1,5 @@
+# -*- coding: cp949 -*- 
+
 #load data
 from datasets import load_dataset
 import pandas as pd
@@ -7,7 +9,11 @@ dataset = load_dataset("nsmc")
 train_df = pd.DataFrame(dataset['train'])
 test_df = pd.DataFrame(dataset['test'])
 
+print("Load Data Done!")
+
 import re
+
+
 
 docs = dataset['train']['document'] + dataset['test']['document']
 label = dataset['train']['label'] + dataset['test']['label']
@@ -20,6 +26,8 @@ import pickle
 okt = Okt()
 
 res = []
+
+import os
 
 if not os.path.isfile('senspos.pickle'):
   for doc in docs:
@@ -38,6 +46,7 @@ else:
     res = pickle.load(f)
 
 
+print("POS Extraction Done!")
 
 # padding
 maxlen = max(len(x) for x in res)
@@ -47,6 +56,7 @@ for i in range(len(res)):
   temp = sen + [" <PAD/>"] * (maxlen - len(sen))
   padded_sens.append(temp)
 
+print("Padding Done!")
 
 # vocab to index
 import nltk
@@ -63,14 +73,17 @@ import numpy as np
 x = np.array([[vocabulary[w] for w in sentence] for sentence in padded_sens])
 y = np.array(label)
 
+print("Indexing Done!")
+
 # word2vec
 from gensim.models import word2vec
 import numpy as np
 
-w2v = word2vec.Word2Vec.load("../data/w2v/ko.bin")
+w2v = word2vec.Word2Vec.load("data\w2v\ko.bin")
 
 w2v = {k: w2v[w] if w in w2v else np.random.uniform(-0.25, 0.25, w2v.vector_size) for w, k in vocabulary.items()}
 
+print("W2V Done!")
 
 # args setting
 embedding_dim = 200
@@ -81,35 +94,24 @@ hidden_dims = 100
 
 batch_size = 50
 num_epochs = 10
-min_word_count = 1
-context = 10
+# min_word_count = 1
+# context = 10
 
 # make dataset to use at training
-try:
-  X_train = np.load('xtrain.npy')
-  y_train = y[:len(train_df)]
-  X_test = np.load('xtest.npy')
-  y_test = y[len(train_df):]
-  
-except:
 
-  X_train = x[:len(train_df)]
-  y_train = y[:len(train_df)]
-  X_test = x[len(train_df):]
-  y_test = y[len(train_df):]
+X_train = x[:len(train_df)]
+y_train = y[:len(train_df)]
+X_test = x[len(train_df):]
+y_test = y[len(train_df):]
 
-  X_train = np.stack([np.stack([w2v[w] for w in sen]) for sen in X_train])
-  X_test = np.stack([np.stack([w2v[w] for w in sen]) for sen in X_test])
-
-  np.save('xtrain', X_train)
-  np.save('xtest', X_test)
+del x, y, tokens, text, word_count, padded_sens, res, docs, label, train_df, test_df, dataset, vocabulary
 
 # setting model and train
 from tensorflow import keras
 
-inp = keras.layers.Input(shape=(X_test.shape[1] ,embedding_dim))
+inp = keras.layers.Input(shape=(X_test.shape[1], ))
 
-m = inp
+m = keras.layers.Embedding(len(vocabulary_inv), embedding_dim, input_length=X_test.shape[1], name="embedding")(inp) # non-static-cnn
 m = keras.layers.Dropout(dropout)(m)
 
 # build conv block
@@ -131,6 +133,11 @@ out = keras.layers.Dense(1, activation="sigmoid")(m)
 
 model = keras.Model(inp, out)
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# add embedding layer for non-static-cnn
+w = np.array([v for v in w2v.values()])
+embedding_layer = model.get_layer("embedding")
+embedding_layer.set_weights([w])
 
 model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs,
           validation_data=(X_test, y_test), verbose=2)
